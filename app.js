d9ea530adb9e2f8b2615bbd6096a39fa205bd6cf
Jonathen <jonathen027@gmail.com>
2023-10-19 15:08:01 +0100

    Added a feature which changes color to black when hovering through grid, also added functionality to grid size and reset button

--- app.js
+++ app.js
@@ -1,17 +1,36 @@
 const container = document.querySelector("#container")
 const box = document.querySelector(".box")
+const sizeOfGridBtn = document.querySelector("#sizeOfGrid")
+const resetBtn = document.querySelector("#reset")
 let gridSize = 5
-
 
 function grid (gridSize) {
     container.style.setProperty("display", "grid")
     container.style.setProperty("grid-template-columns", `repeat(${gridSize}, 1fr)`)
     for(let i = 0; i < (gridSize * gridSize); i++){
-        const gridItem = document.createElement("div")      
+        const gridItem = document.createElement("div") 
         gridItem.classList.add("box")
         container.append(gridItem)
-    }
+        gridItem.addEventListener("mouseover", ()=>{
+        gridItem.style.setProperty("background-color", "black")
+    }) 
+  }
 }
 
-grid(10)
+function removeGrid() {
+    const removeGridItems = document.querySelectorAll(".box")
+    removeGridItems.forEach((gridItem)=>{gridItem.remove()})
+ }
 
+resetBtn.addEventListener("click", () => {
+    removeGrid()
+    grid(gridSize)
+  })
+
+sizeOfGridBtn.addEventListener("click", () => {
+    removeGrid()
+    gridSize = prompt("")
+    grid(gridSize)
+  })
+
+grid(5)
